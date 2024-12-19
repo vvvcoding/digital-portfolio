627f1b9b616d51ea9bd7b1be7b15610cb6ea14e2
vvvcoding <vvncodework@outlook.com>
2024-12-19 00:57:20 -0600

    Add files via upload
    Digital Portfolio that displays all the projects I've done and code I've written. Still a work in progress.

---
+++ indexScript.js
@@ -0,0 +1,51 @@
+/* Code for scrolling down to a certain point on the page */
+function scrollToElement(elementSelector, instance = 0) {
+    // Select all elements that match the given selector
+    const elements = document.querySelectorAll(elementSelector);
+    // Check if there are elements matching the selector and if the requested instance exists
+    if (elements.length > instance) {
+        // Scroll to the specified instance of the element
+        elements[instance].scrollIntoView({behavior: 'smooth'});
+    }
+}
+
+const link1 = document.getElementById("link1");
+const link2 = document.getElementById("link2");
+const link3 = document.getElementById("link3");
+
+link1.addEventListener('click', () => {
+    scrollToElement('.header');
+});
+
+link2.addEventListener('click', () => {
+    // Scroll to the second element with "header" class
+    scrollToElement('.header', 1);
+});
+
+link3.addEventListener('click', () => {
+    scrollToElement('.column');
+});
+
+
+/* Code for going back to the top of the page */
+// Get the button:
+let mybutton = document.getElementById("link4");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth' // Enables smooth scrolling
+    });
+}
